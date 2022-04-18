c760aefe786962d423a7a1471831e342d9b86e2c
dino3616 <suraimu3616@outlook.jp>
2022-04-19 04:27:44 +0900

    feat: ✨ ランチャーを実装.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,27 @@
 #![windows_subsystem = "windows"]
 
-fn main() {
+use anyhow::Result;
+use std::process::Command;
+
+fn main() -> Result<()> {
     tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .invoke_handler(tauri::generate_handler![launch_game])
+        .run(tauri::generate_context!())?;
+
+    Ok(())
+}
+
+#[tauri::command]
+fn launch_game() -> Result<(), String> {
+    let target = "./e-hanoi.exe";
+    match Command::new(target).spawn() {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            let err_msg = format!(
+                "Error: Could not launch e-Hanoi.\ndetails...{}\n指定されたファイル: {}",
+                err, target
+            );
+            Err(err_msg)
+        }
+    }
 }
