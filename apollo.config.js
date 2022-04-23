aee821c982308fa86f2edb5c6d6cdef09f3b4b27
dino3616 <suraimu3616@outlook.jp>
2022-04-23 21:17:47 +0900

    chore: 🔧 Apolloの設定ファイルを作成.

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    includes: ['./src/**/*.{ts, tsx}'],
+    service: {
+      name: 'e-hanoi',
+      url: 'https://e-hanoi.herokuapp.com/graphql',
+    },
+  },
+};
